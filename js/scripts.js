283e96070fd0b28dc058c91f69408cd876eb102b
Eduardo Marques <edumfariabraga@gmail.com>
2020-05-20 21:34:31 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+function showInformatica() {
+
+document.getElementById("inf01").style.display = "block";
+document.getElementById("inf02").style.display = "block";
+document.getElementById("inf03").style.display = "block";
+
+}
+
+function showLicitacao() {
+
+document.getElementById("lic01").style.display = "block";
+document.getElementById("lic02").style.display = "block";
+document.getElementById("lic03").style.display = "block";
+
+}
+
+function showS_informacao() {
+
+document.getElementById("sinf01").style.display = "block";
+document.getElementById("sinf02").style.display = "block";
+document.getElementById("sinf03").style.display = "block";
+
+}
+
+function redirectSSL() {
+
+
+
+}
